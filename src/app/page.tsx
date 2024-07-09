70ce64dc6ccb1d61b65896fccaa8258ec278b9f7
Hiperultimate <hiperultimatevegito@gmail.com>
2024-07-09 12:29:35 +0530

    Nextjs Initialization

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-between pt-24">
+      <div className="font-mono text-sm">
+        Inventory Management System
+      </div>
+    </main>
+  );
+}
